7639f8c607951a0a9440e1be4e7f665032b25112
chase71 <chase.corvette@icloud.com>
2025-02-27 12:40:01 -0800

    accident

--- assets/js/adventureGame/GameLevelPrisonEscape.js
+++ assets/js/adventureGame/GameLevelPrisonEscape.js
@@ -20,33 +20,9 @@ class GameLevelPrisonEscape {
       src: image_src_desert,
       pixels: { height: 168, width: 300 }
     };
-
+3
     // Player data for MC
-    const sprite_src_chillguy = `${path}/assets/js/adventureGame/MC.png`;
-    const CHILLGUY_SCALE_FACTOR = 5;
-    const sprite_data_chillguy = {
-      id: 'Chill Guy',
-      greeting: "Hi, I am Chill Guy, Currently in prison, Unfortunatly I need to break out!",
-      src: sprite_src_chillguy,
-      SCALE_FACTOR: CHILLGUY_SCALE_FACTOR,
-      STEP_FACTOR: 1000,
-      ANIMATION_RATE: 50,
-      INIT_POSITION: { x: 0, y: height - (height / CHILLGUY_SCALE_FACTOR) },
-      pixels: { height: 950, width: 644 },
-      orientation: { rows: 4, columns: 4 },
-      frameSize: { width: 161, height: 237 },
-      down: { row: 0, start: 0, columns: 3 },
-      downRight: {row: 1, start: 0, columns: 3, rotate: Math.PI/16 },
-        downLeft: {row: 2, start: 0, columns: 3, rotate: -Math.PI/16 },
-      left: { row: 2, start: 0, columns: 3 },
-      right: { row: 3, start: 0, columns: 3 },
-      up: { row: 1, start: 0, columns: 3 },
-      upLeft: {row: 2, start: 0, columns: 3, rotate: Math.PI/16 },
-        upRight: {row: 1, start: 0, columns: 3, rotate: -Math.PI/16 },
-      hitbox: { widthPercentage: 0.45, heightPercentage: 0.2 },
-      keypress: { up: 87, left: 65, down: 83, right: 68 }
-    };
-
+    
     // NPC Data
     const npcs = [
       {
