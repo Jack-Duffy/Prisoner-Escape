6afebc0226d68965955a195086a534e67543676d
Jack-Duffy <madkatjack@gmail.com>
2025-02-28 11:53:51 -0800

    commit

--- assets/js/adventureGame/GameLevelPrisonEscape.js
+++ assets/js/adventureGame/GameLevelPrisonEscape.js
@@ -21,33 +21,31 @@ class GameLevelPrisonEscape {
       pixels: { height: 168, width: 300 }
     };
 
-    // Player data for MC
     const sprite_src_chillguy = `${path}/assets/js/adventureGame/MainCharecter.png`;
-    const CHILLGUY_SCALE_FACTOR =  10;
+    const CHILLGUY_SCALE_FACTOR = 10;
     const sprite_data_chillguy = {
     id: 'Chill Guy',
-    greeting: "Hi, I am Chill Guy, Currently in prison, Unfortunatly I need to break out!",
+    greeting: "Hi, I am Chill Guy, Currently in prison, Unfortunately I need to break out!",
     src: sprite_src_chillguy,
     SCALE_FACTOR: CHILLGUY_SCALE_FACTOR,
-    STEP_FACTOR: 1000,
-    ANIMATION_RATE: 50,
+    STEP_FACTOR: 2000,
+    ANIMATION_RATE: 10,
     INIT_POSITION: { x: 0, y: height - (height / CHILLGUY_SCALE_FACTOR) },
-    pixels: { height: 950, width: 644 }, 
+    pixels: { height: 760, width: 200 }, 
     orientation: { rows: 4, columns: 4 }, 
-    frameSize: { width: 161, height: 237 }, 
+    frameSize: { width: 100, height: 190 }, 
     down: { row: 0, start: 0, columns: 3 }, 
-    downRight: { row: 1, start: 0, columns: 3, rotate: Math.PI / 16 }, 
-    downLeft: { row: 2, start: 0, columns: 3, rotate: -Math.PI / 16 }, 
+    up: { row: 1, start: 0, columns: 3 }, 
     left: { row: 2, start: 0, columns: 3 }, 
     right: { row: 3, start: 0, columns: 3 }, 
-    up: { row: 1, start: 0, columns: 3 }, 
-    upLeft: { row: 2, start: 0, columns: 3, rotate: Math.PI / 16 }, 
+    downRight: { row: 0, start: 0, columns: 3, rotate: Math.PI / 16 }, 
+    downLeft: { row: 0, start: 0, columns: 3, rotate: -Math.PI / 16 }, 
+    upLeft: { row: 1, start: 0, columns: 3, rotate: Math.PI / 16 }, 
     upRight: { row: 1, start: 0, columns: 3, rotate: -Math.PI / 16 }, 
     hitbox: { widthPercentage: 0.45, heightPercentage: 0.2 }, 
     keypress: { up: 87, left: 65, down: 83, right: 68 }
+};
 
-
-    };
 
 
     // NPC Data
