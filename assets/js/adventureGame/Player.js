d73f4c6113bbea93cf0260fa67d66e1b05b77ee1
chase71 <chase.corvette@icloud.com>
2025-03-03 20:59:33 -0800

    reset to original

--- assets/js/adventureGame/Player.js
+++ assets/js/adventureGame/Player.js
@@ -25,16 +25,15 @@ class Player extends Character {
     constructor(data = null) {
         super(data);
         this.keypress = data?.keypress || {up: 87, left: 65, down: 83, right: 68};
-        this.activeKeys = new Set();
-        this.speedMultiplier = 6.5; // Further increased speed
         this.bindEventListeners();
     }
+
 
     /**
      * Binds key event listeners to handle object movement.
      * 
      * This method binds keydown and keyup event listeners to handle object movement.
-     * The .bind(this) method ensures that 'this' refers to the object.
+     * The .bind(this) method ensures that 'this' refers to the object object.
      */
     bindEventListeners() {
         addEventListener('keydown', this.handleKeyDown.bind(this));
@@ -42,32 +41,50 @@ class Player extends Character {
     }
 
     handleKeyDown({ keyCode }) {
-        this.activeKeys.add(keyCode);
-        this.updateVelocity();
+        switch (keyCode) {
+            case this.keypress.up:
+                this.velocity.y -= this.yVelocity;
+                this.direction = 'up';
+                break;
+            case this.keypress.left:
+                this.velocity.x -= this.xVelocity;
+                this.direction = 'left';
+                break;
+            case this.keypress.down:
+                this.velocity.y += this.yVelocity;
+                this.direction = 'down';
+                break;
+            case this.keypress.right:
+                this.velocity.x += this.xVelocity;
+                this.direction = 'right';
+                break;
+        }
     }
 
+    /**
+     * Handles key up events to stop the player's velocity.
+     * 
+     * This method stops the player's velocity based on the key released.
+     * 
+     * @param {Object} event - The keyup event object.
+     */
     handleKeyUp({ keyCode }) {
-        this.activeKeys.delete(keyCode);
-        this.updateVelocity();
-    }
-
-    updateVelocity() {
-        this.velocity.x = 0;
-        this.velocity.y = 0;
-
-        if (this.activeKeys.has(this.keypress.up)) {
-            this.velocity.y -= this.yVelocity * this.speedMultiplier;
-        }
-        if (this.activeKeys.has(this.keypress.down)) {
-            this.velocity.y += this.yVelocity * this.speedMultiplier;
-        }
-        if (this.activeKeys.has(this.keypress.left)) {
-            this.velocity.x -= this.xVelocity * this.speedMultiplier;
-        }
-        if (this.activeKeys.has(this.keypress.right)) {
-            this.velocity.x += this.xVelocity * this.speedMultiplier;
+        switch (keyCode) {
+            case this.keypress.up:
+                this.velocity.y = 0;
+                break;
+            case this.keypress.left:
+                this.velocity.x = 0;
+                break;
+            case this.keypress.down: 
+                this.velocity.y = 0;
+                break;
+            case this.keypress.right: 
+                this.velocity.x = 0;
+                break;
         }
     }
+
 }
 
 export default Player;
